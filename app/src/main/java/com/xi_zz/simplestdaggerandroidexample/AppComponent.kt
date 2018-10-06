638a76a974d6e3568b06b1e37bb8292eb4c0a664
GC-Xi <xi@gc.com>
2018-10-06 18:32:31 -0400

    Step 4. Create the AppComponent.

---
+++ app/src/main/java/com/xi_zz/simplestdaggerandroidexample/AppComponent.kt
@@ -0,0 +1,23 @@
+package com.xi_zz.simplestdaggerandroidexample
+
+import android.app.Application
+
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import dagger.android.support.DaggerApplication
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidSupportInjectionModule::class, ActivityBuilder::class])
+interface AppComponent : AndroidInjector<DaggerApplication> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
