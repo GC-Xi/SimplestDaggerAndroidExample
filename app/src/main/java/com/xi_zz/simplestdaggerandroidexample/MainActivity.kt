003dbf46a07a44bf09423cf15f392023a8b8876e
GC-Xi <xi@gc.com>
2018-10-06 18:47:39 -0400

    Step 6. Using Dependency Inject in the `Activity`

--- app/src/main/java/com/xi_zz/simplestdaggerandroidexample/MainActivity.kt
+++ app/src/main/java/com/xi_zz/simplestdaggerandroidexample/MainActivity.kt
@@ -2,11 +2,21 @@ package com.xi_zz.simplestdaggerandroidexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.textView
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var textService: TextService
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = textService.repeat3Times(R.string.app_name) // Should display the app name three times
     }
 }
